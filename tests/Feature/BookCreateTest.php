418a2937e1d279d4472a2ba0706de945fea7111d
mahdi.aslami <mahdiaslamikhavari@gmail.com>
2021-10-07 22:58:13 +0330

    Updated test names.

--- tests/Feature/BookCreateTest.php
+++ tests/Feature/BookCreateTest.php
@@ -6,14 +6,14 @@ use Tests\TestCase;
 
 class BookCreateTest extends TestCase
 {
-    public function test_show_create_book_form()
+    public function test_show_create_book_page()
     {
         return $this->get('/books/create')
             ->assertOk()
             ->assertViewIs('books.create');
     }
 
-    /** @depends test_show_create_book_form */
+    /** @depends test_show_create_book_page */
     public function test_it_has_link_to_all_books_page($response)
     {
         $this->assertHasLink($response, '/books');
