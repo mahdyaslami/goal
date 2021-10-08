c8ad5bbdf24a5636e46bdd1e37289e6b38ae5adc
mahdi.aslami <mahdiaslamikhavari@gmail.com>
2021-10-08 15:49:50 +0330

    Added test_show_edit_book_page.

---
+++ tests/Feature/Book/BookEditTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature\Book;
+
+use App\Models\Book;
+use Tests\TestCase;
+
+class BookEditTest extends TestCase
+{
+    public function test_show_edit_book_page()
+    {
+        $book = Book::factory()->create();
+
+        $this->get($book->pathToEdit())
+            ->assertOk();
+    }
+}
