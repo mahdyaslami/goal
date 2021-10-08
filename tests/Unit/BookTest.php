38c8de8f53267ed2a2fb19078811619bd2f93dc3
mahdi.aslami <mahdiaslamikhavari@gmail.com>
2021-10-08 15:49:32 +0330

    Added tests to pathToEdit

---
+++ tests/Unit/BookTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Book;
+use Tests\TestCase;
+
+class BookTest extends TestCase
+{
+    public function test_it_get_edit_route()
+    {
+        $book = Book::factory()->create();
+
+        $this->assertEquals(
+            route('book-edit', ['book' => $book->id]),
+            $book->pathToEdit()
+        );
+    }
+}
