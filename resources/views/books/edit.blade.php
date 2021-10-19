d93f5b41f2f52c8fa783aeaa2e0879a33bd9c0b5
mahdi.aslami <mahdiaslamikhavari@gmail.com>
2021-10-19 17:47:53 +0330

    Create form.blade to remve duplication of book form

--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -9,34 +9,10 @@
 </head>
 
 <body>
-    <form action="{{ $book->pathToUpdate() }}" method="POST">
-        @csrf
-        @method('PUT')
-
-        <x-forms.input 
-            id="title" 
-            name="title" 
-            label="عنوان" 
-            type="text" 
-            maxlength="255" 
-            value="{{ $book->title }}"
-            require 
-        />
-
-        <x-forms.input
-            id="page_count"
-            name="page_count"
-            label="تعداد صفحات"
-            type="number"
-            min="1"
-            max="1000"
-            value="{{ $book->page_count }}"
-            require
-        />
-
-        <button type="submit">ذخیره</button>
-        <a href="/books">انصراف</a>
-    </form>
+    @include('books.form', [
+        'action' => $book->pathToUpdate(),
+        'method' => 'PUT'
+    ])
 </body>
 
 </html>
