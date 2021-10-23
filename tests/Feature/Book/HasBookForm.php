36bbe93efc0ea963b15bfca253621f54e11d5f13
mahdi.aslami <mahdiaslamikhavari@gmail.com>
2021-10-23 18:31:11 +0330

    Only create book form has input for step_count.

--- tests/Feature/Book/HasBookForm.php
+++ tests/Feature/Book/HasBookForm.php
@@ -30,19 +30,6 @@ trait HasBookForm
     }
 
     /** @depends test_has_form */
-    protected function test_has_input_for_step_count($args)
-    {
-        extract($args);
-
-        $this->assertDomHasInput($response, 'number', 'step_count', [
-            'value' => $book ? $book->page_count : '',
-            'min' => 1,
-            'max' => 1000,
-            'require' => 'require'
-        ]);
-    }
-
-    /** @depends test_has_form */
     protected function test_has_link_to_books_page()
     {
         extract($args);
